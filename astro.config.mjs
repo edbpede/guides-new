c607ba7e6e6ab728bbcf872894b5c9b8ff4fe130
edbpede <144238562+edbpede@users.noreply.github.com>
2025-03-11 19:11:04 +0100

    fix: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://guides.edbpede.net', // Add your actual site URL here
   integrations: [
     starlight({
       title: 'EDB Guides',
